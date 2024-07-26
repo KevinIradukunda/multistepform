bbb08cde220ffc99fcfbdfdf9a5402dfe4a0494a
Iradukunda12 <iradukundakvn8@gmail.com>
2024-07-26 11:28:05 +0200

    changed the name folder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { StepOneComponent } from './components/step-one/step-one.component';
-import { StepTwoComponent } from './components/step-two/step-two.component';
-import { StepThreeComponent } from './components/step-three/step-three.component';
-import { StepFourComponent } from './components/step-four/step-four.component';
-import { ThankYouComponent } from './components/thank-you/thank-you.component';
+import { SidebarComponent } from './pages/sidebar/sidebar.component';
+import { StepOneComponent } from './pages/step-one/step-one.component';
+import { StepTwoComponent } from './pages/step-two/step-two.component';
+import { StepThreeComponent } from './pages/step-three/step-three.component';
+import { StepFourComponent } from './pages/step-four/step-four.component';
+import { ThankYouComponent } from './pages/thank-you/thank-you.component';
 import { FormDataService } from './Services/form-data.service';
 
 @Component({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { StepOneComponent } from './components/step-one/step-one.component';
-import { StepTwoComponent } from './components/step-two/step-two.component';
-import { PlanCardComponent } from './components/plan-card/plan-card.component';
+import { SidebarComponent } from './pages/sidebar/sidebar.component';
+import { StepOneComponent } from './pages/step-one/step-one.component';
+import { StepTwoComponent } from './pages/step-two/step-two.component';
+import { PlanCardComponent } from './pages/plan-card/plan-card.component';
 import { FormDataService } from './Services/form-data.service';
 
 @NgModule({
